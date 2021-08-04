984e41b2f396a620275fecd6edde38fdb2d7a95f
jyliao369 <jyliao369@gmail.com>
2021-08-04 15:05:28 -0400

    setup the format for the readme file

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -12,9 +12,53 @@ function renderLicenseSection(license) {}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return `## Project Title: ${data.title}
 
-`;
+  ## Description: 
+  
+  ## Table of Contents
+      * [Installation]
+      * [Usage]
+      * [Credits]
+      * [License]
+      * [Contributors](#contributing)
+      * [Tests](#tests)
+      * [Questions](#questions)
+  
+  ## Installation
+  -- What technologies did the application require in order to run smoothly and effectively?--
+
+  The following dependencies were installed to run the application properly: 
+
+  ## Usage
+  -- How do you use this application? --
+
+  In order to use this app, you will need: 
+
+  ## License
+  -- Are there any license the application has? --
+
+  The project is under the ____ license
+
+  ## Contributors
+  -- Where there any contributors to the application/project --
+
+  Contributors that help create this application are:
+
+  ## Tests
+  -- How can we test the application --
+
+  The following is needed in order to run tests:
+
+  ## Questions
+  -- Do you have any questions? --
+
+  If you do, contact me at:
+
+  -- Want to see the repo of the project__
+
+  The repository can be accessed here: 
+  `;
 }
 
 module.exports = generateMarkdown;
